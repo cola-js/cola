03ffa62bcae6f53f50a0fe5e51737c2e259d9e1c
Jony <niceue@live.com>
2020-03-10 13:08:21 +0800

    优化支付宝 config 提取

--- packages/target/lib/platforms/alipay/codegen/page.json.js
+++ packages/target/lib/platforms/alipay/codegen/page.json.js
@@ -4,13 +4,36 @@ const {
   convertPageConfig
 } = require( '../convert-config' )
 
+let appConfig = {}
+
 module.exports = function ( { config = {}, file } ) {
   const _config = composePlatformConfig( config, 'alipay' )
   let converted = {}
   if (file === 'app') {
-    converted = convertAppConfig( _config )
+    converted = appConfig = convertAppConfig( _config )
+    // 全局自定义导航栏
+    if (converted.window.navigationStyle === 'custom') {
+      converted.window.titlePenetrate = 'NO'
+      converted.window.transparentTitle = 'none'
+    }
+    // 兼容 subpakages 字段
+    if (converted.subpackages && !converted.subPackages) {
+      converted.subPackages = converted.subpackages
+      delete converted.subpackages
+    }
   } else {
     converted = convertPageConfig( _config )
+    // 页面自定义导航栏
+    if (
+      (appConfig.window.navigationStyle === 'custom' && converted.navigationStyle !== 'default') ||
+      converted.navigationStyle === 'custom'
+    ) {
+      converted.defaultTitle = ''
+      converted.titlePenetrate = 'YES'
+      if (!converted.transparentTitle) {
+        converted.transparentTitle = 'always'
+      }
+    }
   }
   return JSON.stringify( converted, 0, 2 )
 }
