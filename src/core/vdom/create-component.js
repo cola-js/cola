4fd97536359dc19bea73f26493ce2ffec9a0e75c
Jony <niceue@live.com>
2020-03-31 17:39:58 +0800

    修复报错：Cannot read property '_isDestroyed' of undefined;

--- src/core/vdom/create-component.js
+++ src/core/vdom/create-component.js
@@ -82,7 +82,7 @@ const componentVNodeHooks = {
 
   destroy (vnode: MountedComponentVNode) {
     const { componentInstance } = vnode
-    if (!componentInstance._isDestroyed) {
+    if (componentInstance && !componentInstance._isDestroyed) {
       if (!vnode.data.keepAlive) {
         componentInstance.$destroy()
       } else {
